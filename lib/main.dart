56e9c833be51984401a9973452cd923b927c8344
anil <anilkumar2000jnv@gmail.com>
2024-12-03 10:12:41 +0530

    updated firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:minimal_chat_app/services/auth/auth_gate.dart';
 import 'package:minimal_chat_app/firebase_options.dart';
 import 'package:minimal_chat_app/themes/theme_provider.dart';
@@ -7,6 +8,16 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load .env variables
+  try {
+    await dotenv.load(fileName: ".env");
+    print("Dotenv initialized: ${dotenv.isInitialized}");
+    print("Loaded env variables: ${dotenv.env}");
+  } catch (e) {
+    print("Error loading .env file: $e");
+  }
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ChangeNotifierProvider(
     create: (context) => ThemeProvider(),
